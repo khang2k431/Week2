1addc86bfeb299e49a14595e64acaaf8680a8dd6
khang2k431 <khangtpdce180788@fpt.edu.vn>
2025-09-15 11:51:43 +0700

    Create auth_middleware.go

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"Week2/utils"
+)
+
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		if auth == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			return
+		}
+		parts := strings.SplitN(auth, " ", 2)
+		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization format must be Bearer {token}"})
+			return
+		}
+		token := parts[1]
+		claims, err := utils.ParseToken(token)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		c.Set("claims", claims)
+		c.Next()
+	}
+}
